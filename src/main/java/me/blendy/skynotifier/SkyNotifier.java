1f5cb84494d88a0e9a14ad47227a62a4b4d8b0a5
BlendyDev <blendydev@gmail.com>
2023-06-06 22:49:06 +0200

    Changed version string and mod description

--- src/main/java/me/blendy/skynotifier/SkyNotifier.java
+++ src/main/java/me/blendy/skynotifier/SkyNotifier.java
@@ -23,7 +23,7 @@ import java.util.Arrays;
 @Mod(modid = SkyNotifier.MODID, version = SkyNotifier.VERSION)
 public class SkyNotifier {
     public static final String MODID = "skynotifier";
-    public static final String VERSION = "1.0";
+    public static final String VERSION = "1.1";
     public static String token = "";
     public static String channelID = "";
     public static String keywords = "";
